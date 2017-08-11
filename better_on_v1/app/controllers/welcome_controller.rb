5b8919b615fd9a8d821bc889062ca9bf776bc743
Michael Zhang <mzhang@betteron.video>
2017-08-11 18:50:33 +0000

    done admin

--- better_on_v1/app/controllers/welcome_controller.rb
+++ better_on_v1/app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
    
-   def login
+   def index
        
    end
     
--- better_on_v1/config/routes.rb
+++ better_on_v1/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#login'
+  root 'welcome#index'
   
   #get 'profile/home', to: 'profile#home'
   
